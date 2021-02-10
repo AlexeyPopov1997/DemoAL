f778667272b4523feee4308373e73bd645e739e7
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-02-10 11:11:31 +0300

    Update ExtensionPublishTool.psm1

--- scripts/ExtensionPublishTool.psm1
+++ scripts/ExtensionPublishTool.psm1
@@ -1,4 +1,5 @@
 # Функция для отмены публикации расширения и зависимых от него
+# [Internal]
 function UnpublishAppAndDependencies($ServerInstance, $ApplicationName) {
     Get-NAVAppInfo -ServerInstance $ServerInstance | Where-Object { 
         (Get-NAVAppInfo -ServerInstance $ServerInstance -Name $_.Name).Dependencies | Where-Object {$_.Name -eq $ApplicationName}
@@ -10,40 +11,46 @@ function UnpublishAppAndDependencies($ServerInstance, $ApplicationName) {
 }
 
 # Функция для отмены публикации и удаления расширения
+# [Comandlet]
 function UninstallAndUnpublish($ServerInstance, $ApplicationName) {
     Uninstall-NAVApp -ServerInstance $ServerInstance -Name $ApplicationName -Force
     UnpublishAppAndDependencies $ServerInstance  $ApplicationName
 }
 
 # Функция для публикации расширения и зависимых от него
-function PublishAppAndDependencies($ServerInstance, $ApplicationName) {
+function PublishAppAndDependencies($ServerInstance, $ApplicationName, $ApplicationPath) {
     Get-NAVAppInfo -ServerInstance $ServerInstance | Where-Object {
         (Get-NAVAppInfo -ServerInstance $ServerInstance -Name $_.Name).Dependencies | Where-Object {$_.Name -eq $ApplicationName} 
     } | ForEach-Object {
         PublishAppAndDependencies $ServerInstance $_.Name
     }
     
-    Publish-NAVApp -Path $ApplicationPath -ServerInstance $ServerInstance -Force -SkipVerification
+    Publish-NAVApp -ServerInstance $ServerInstance -Path $ApplicationPath -Force -SkipVerification
 }
 
 # Функция для публикации и установки расширения
-function PublishAndInstall($ServerInstance, $ApplicationName) {
-    PublishAppAndDependencies $ServerInstance  $ApplicationName
-    Install-NAVApp -ServerInstance $ServerInstanceName -Name $ApplicationName
+function PublishAndInstall($ServerInstance, $ApplicationName, $ApplicationPath) {
+    PublishAppAndDependencies $ServerInstance  $ApplicationName $ApplicationPath
+    Install-NAVApp -ServerInstance $ServerInstance -Path $ApplicationPath -Tenant "Tenant1" 
 }
 
 # Командлет для перепубликации и установки расширения
-# Пример вызова: RepublishApplication -ServerInstanceName BC170 -ApplicationPath "C:\ALPOPOV\AL\DemoAL\ALPOPOV_Customer Extension_1.0.0.0.app"
+# Пример вызова: RepublishApplication -ServerInstance BC170 -ApplicationName "Customer Extension" -ApplicationPath "C:\ALPOPOV\AL\DemoAL\ALPOPOV_Customer Extension_1.0.0.0.app"
 function RepublishApplication {
-    param([string]$ServerInstanceName, [string]$ApplicationPath)
+    param([string]$ServerInstance, [string]$ApplicationName, [string]$ApplicationPath)
     
-    $NAVInfo = Get-NAVAppInfo -ServerInstance $ServerInstanceName | Where-Object {$_.Name -eq "Customer Extension"}
+    $NAVInfo = Get-NAVAppInfo -ServerInstance $ServerInstance | Where-Object {$_.Name -eq $ApplicationName}
     if($NAVInfo) {
-        UninstallAndUnpublish -ServerInstance $ServerInstanceName -ApplicationName "Customer Extension"
+        UninstallAndUnpublish -ServerInstance $ServerInstance -ApplicationName $ApplicationName
     }
     
-    $null = PublishAndInstall -ServerInstance $ServerInstanceName -ApplicationName "Customer Extension"
+    $null = PublishAndInstall -ServerInstance $ServerInstance -ApplicationName $ApplicationName -ApplicationPath $ApplicationPath
 }
+
+
+###############################################################################################################################################################
+***************************************************************************************************************************************************************
+###############################################################################################################################################################
 
 # Командлет для отмены публикации расширения
 # Пример вызова: UnpublishApplication -ServerInstanceName BC170
