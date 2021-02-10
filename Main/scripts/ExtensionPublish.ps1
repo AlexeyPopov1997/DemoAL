0fbe2928ac413d6b5ed248ee0a9eb4b7b9df466c
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-02-10 16:40:16 +0300

    Add ExtensionPublish.ps1

---
+++ Main/scripts/ExtensionPublish.ps1
@@ -0,0 +1,18 @@
+$userName = $env:username
+
+git pull
+
+cd "C:\Users\" + "$userName" + "\.vscode\extensions\ms-dynamics-smb.al-6.1.397164\bin\"
+
+./alc.exe /project:C:\ALPOPOV\AL\DemoAL\Main /packagecachepath:C:\ALPOPOV\AL\DemoAL\Main\.alpackages
+
+cd scripts
+
+./NavAdminTool.ps1
+
+Import-Module ".\ExtensionAdminTool.psm1" -Verbose
+
+RepublishApp -ServerInstanceName BC170 -AppPath ".\ALPOPOV_Customer Extension_1.0.0.0.app"
+
+git push origin DEV 
+
