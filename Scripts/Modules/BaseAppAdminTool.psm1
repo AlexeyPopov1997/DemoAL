d73259b5a4a4e7d4a811d639a9e21ca39d57c016
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-02-14 00:59:43 +0300

    Refuctoring

--- Scripts/Modules/BaseAppAdminTool.psm1
+++ Scripts/Modules/BaseAppAdminTool.psm1
@@ -36,7 +36,7 @@ function PublishAndInstall($ServerInstance, $ApplicationName)
 }
 
 # Командлет для перепубликации и установки базового приложения
-# Пример вызова: RepublishBaseApp -ServerInstanceName BC170 -BaseAppPath "C:\ALPOPOV\AL\DemoAL\AL\BaseApplication\Microsoft_Base Application_17.1.18256.18792.app"
+# Пример вызова: RepublishBaseApp -ServerInstanceName BC170 -BaseAppPath "C:\ALPOPOV\AL\DemoAL\AL\BaseApp\Microsoft_Base Application_17.1.18256.18792.app"
 function RepublishBaseApp {
 param([string]$ServerInstanceName, [string]$BaseAppPath)
 
